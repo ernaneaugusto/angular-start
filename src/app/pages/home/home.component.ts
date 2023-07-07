1de842add0c60c3accf80e874f248e2b7e3ae8e3
Ernane Augusto <ernaneatcipriano@gmail.com>
2023-07-07 17:17:48 -0300

    layout pagina Home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  public form: FormGroup = new FormGroup({
+    n1: new FormControl('', [Validators.required]),
+    n2: new FormControl('', [Validators.required]),
+    n3: new FormControl('', [Validators.required]),
+    n4: new FormControl('', [Validators.required]),
+    n5: new FormControl('', [Validators.required]),
+    n6: new FormControl('', [Validators.required]),
+    n7: new FormControl('', [Validators.required]),
+    n8: new FormControl('', [Validators.required]),
+    n9: new FormControl('', [Validators.required]),
+    n10: new FormControl('', [Validators.required]),
+    n11: new FormControl('', [Validators.required]),
+    n12: new FormControl('', [Validators.required]),
+    n13: new FormControl('', [Validators.required]),
+    n14: new FormControl('', [Validators.required]),
+    n15: new FormControl('', [Validators.required]),
+  });
 
+  public submit(): void {
+    
+  }
 }
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes = [
   { path: '', component: HomeComponent }
@@ -14,6 +15,7 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
+    ReactiveFormsModule
   ],
   exports: [
     HomeComponent
