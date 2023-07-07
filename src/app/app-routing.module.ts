54cc9f5ad3c7bbf42c48982af1b4033c69b5db42
Ernane Augusto <ernaneatcipriano@gmail.com>
2023-07-07 16:37:58 -0300

    separacao em modulos e criacao paginas Home e Erro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeModule } from './pages/home/home.module';
+import { ErrorModule } from './pages/error/error.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: '**',
+    loadChildren: () => import('./pages/error/error.module').then(m => m.ErrorModule)
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(
+      routes,
+      // { enableTracing: true }
+    )
+  ],
+  exports: [
+    RouterModule,
+    HttpClientModule,
+    HomeModule,
+    ErrorModule,
+  ]
 })
 export class AppRoutingModule { }
